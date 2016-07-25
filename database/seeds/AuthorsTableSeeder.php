b14903eae840a561979d2bee6677f1be133a1cd1
Amrit Twanabasu <amrit.cas@gmail.com>
2016-07-25 14:11:26 +0545

    Fix out of bounds count issue in seeder

--- database/seeds/AuthorsTableSeeder.php
+++ database/seeds/AuthorsTableSeeder.php
@@ -16,7 +16,7 @@ class AuthorsTableSeeder extends Seeder {
         $authors = factory(App\Models\Author::class)->times($faker->numberBetween(1,15))->create();
 
         foreach($books as $book) {
-            $book->authors()->attach($faker->randomElements($authors->lists('id')->toArray(),$faker->numberBetween(1,5)));
+            $book->authors()->attach($faker->randomElements($authors->lists('id')->toArray(),$faker->numberBetween(1,$authors->count())));
         }
     }
 }
