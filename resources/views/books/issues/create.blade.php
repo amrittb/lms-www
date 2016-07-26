cf96c011ed97c21fe851849aaa152b78b751fe65
Amrit Twanabasu <amrit.cas@gmail.com>
2016-07-26 18:07:28 +0545

    Update issue page styles

--- resources/views/books/issues/create.blade.php
+++ resources/views/books/issues/create.blade.php
@@ -29,77 +29,84 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <h3 class="text--center">Issue a book copy</h3>
-
-                <div>
-                    <p>
-                        <strong>Book Title: </strong> {{ $bookCopy->book_name }}
-                    </p>
-                    <p>
-                        <strong>ISBN: </strong> {{ $bookCopy->isbn }}
-                    </p>
-                    <p>
-                        <strong>Edition: </strong> {{ $bookCopy->edition }}
-                    </p>
-                    <p>
-                        <strong>Publication: </strong> {{ $bookCopy->publication_name }}
-                    </p>
-                    <p>
-                        <strong>Category: </strong> {{ $bookCopy->category_name }}
-                    </p>
-                    <p>
-                        <strong>Copy ID: </strong> {{ $bookCopy->copy_id }}
-                    </p>
-                    <p>
-                        <strong>Status: </strong> {{ ($bookCopy->is_issued == 0)?"Available":"Issued" }}
-                    </p>
-                </div>
+                <h2 class="text--center">Issue a book copy</h2>
 
                 @include('partials.messagebag')
 
-                <form action="{{ route('books.copies.issue',['books' => $bookCopy->id,'copies' => $bookCopy->copy_id]) }}" method="POST" class="form-horizontal">
-                    {{ csrf_field() }}
+                <div class="row">
+                    <div class="jumbotron">
+                        <p>
+                            <strong>Book Title: </strong> {{ $bookCopy->book_name }}
+                        </p>
+                        <p>
+                            <strong>ISBN: </strong> {{ $bookCopy->isbn }}
+                        </p>
+                        <p>
+                            <strong>Edition: </strong> {{ ordinal_suffix($bookCopy->edition) }}
+                        </p>
+                        <p>
+                            <strong>Publication: </strong> {{ $bookCopy->publication_name }}
+                        </p>
+                        <p>
+                            <strong>Category: </strong> {{ $bookCopy->category_name }}
+                        </p>
+                        <p>
+                            <strong>Copy ID: </strong> {{ $bookCopy->copy_id }}
+                        </p>
+                        <p>
+                            <strong>Status: </strong> {{ ($bookCopy->is_issued == 0)?"Available":"Issued" }}
+                        </p>
+                    </div>
+                </div>
 
-                    <div class="form-group{{ $errors->has('member_id') ? ' has-error' : '' }}">
-                        <div class="col-md-6 form-inline">
-                            @if($bookCopy->is_issued == 0)
-                                <label for="member-id" class="control-label">Member: </label>
-                                <select id="member-id" class="form-control chosen-select" name="member_id">
-                                    @foreach($members as $member)
-                                        <option value="{{ $member->id }}">
-                                            {{ $member->first_name.' '.$member->middle_name.' '.$member->last_name }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @if ($errors->has('member_id'))
-                                    <span class="help-block">
+
+                <div class="row">
+                    <div class="jumbotron">
+                        <form action="{{ route('books.copies.issue',['books' => $bookCopy->id,'copies' => $bookCopy->copy_id]) }}" method="POST" class="form-horizontal">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('member_id') ? ' has-error' : '' }}">
+                                <div class="col-md-6 form-inline">
+                                    @if($bookCopy->is_issued == 0)
+                                        <label for="member-id" class="control-label">Member: </label>
+                                        <select id="member-id" class="form-control chosen-select" name="member_id">
+                                            @foreach($members as $member)
+                                                <option value="{{ $member->id }}">
+                                                    {{ $member->first_name.' '.$member->middle_name.' '.$member->last_name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                        @if ($errors->has('member_id'))
+                                            <span class="help-block">
                                         <strong>{{ $errors->first('member_id') }}</strong>
                                     </span>
-                                @endif
+                                        @endif
+
+                                        <button type="submit" class="btn btn-primary">
+                                            Issue this copy
+                                        </button>
+                                    @else
+                                        <input type="hidden" name="parent_id" value="{{ $transactions->first()->id }}">
+                                        <input type="hidden" name="member_id" value="{{ $transactions->first()->member_id }}">
+                                        <button type="submit" class="btn btn-primary">
+                                            Renew this copy
+                                        </button>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
+
+                        @if($bookCopy->is_issued == 1)
+                            <form method="post" action="{{ route('books.copies.issue.return',['books' => $bookCopy->id,'copies' => $bookCopy->copy_id,'issues' => $transactions->first()->id]) }}">
+                                {{ csrf_field() }}
 
                                 <button type="submit" class="btn btn-primary">
-                                    Issue Book Copy to the user
+                                    Return this copy
                                 </button>
-                            @else
-                                <input type="hidden" name="parent_id" value="{{ $transactions->first()->id }}">
-                                <input type="hidden" name="member_id" value="{{ $transactions->first()->member_id }}">
-                                <button type="submit" class="btn btn-primary">
-                                    Renew Book
-                                </button>
-                            @endif
-                        </div>
+                            </form>
+                        @endif
                     </div>
-                </form>
-
-                @if($bookCopy->is_issued == 1)
-                    <form method="post" action="{{ route('books.copies.issue.return',['books' => $bookCopy->id,'copies' => $bookCopy->copy_id,'issues' => $transactions->first()->id]) }}">
-                        {{ csrf_field() }}
-
-                        <button type="submit" class="btn btn-primary">
-                            Return Copy
-                        </button>
-                    </form>
-                @endif
+                </div>
 
                 <br>
                 <br>
@@ -143,11 +150,13 @@
                             </table>
                         </div>
                     @else
-                        <br>
-                        <br>
-                        <h4 class="text--center">
-                            There are no transactions for this book copy.
-                        </h4>
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h4 class="text--center">
+                                    There are no transactions for this book copy.
+                                </h4><br><br>
+                            </div>
+                        </div>
                     @endif
                 </div>
             </div>
